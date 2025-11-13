ae936029409aa0dbf0edb665238df9fa42b67f0e
othmane <lheothmane@gmail.com>
2025-11-13 21:17:05 +0100

    add repo  event and guest

---
+++ src/main/java/com/smart/event/back_end/database/repositories/EventRepository.java
@@ -0,0 +1,9 @@
+package com.smart.event.back_end.database.repositories;
+
+import com.smart.event.back_end.database.entities.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository  extends JpaRepository<Event,Long> {
+}
