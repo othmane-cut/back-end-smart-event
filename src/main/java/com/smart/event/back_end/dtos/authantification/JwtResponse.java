d6059f246f810c537d4e0f9b3ed5cd39f1a9147e
othmane <lheothmane@gmail.com>
2025-11-14 23:29:19 +0100

    creat jwt response

---
+++ src/main/java/com/smart/event/back_end/dtos/authantification/JwtResponse.java
@@ -0,0 +1,13 @@
+package com.smart.event.back_end.dtos.authantification;
+
+public class JwtResponse {
+    private String token;
+
+    public JwtResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
